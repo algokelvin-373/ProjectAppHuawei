a4a44ebd95cceb3152226d361a044e840d1dcedc
algokelvin-373 <kelvinht373@gmail.com>
2025-02-25 14:49:21 +0700

    Set Page UI Authorization and Flow

--- SehatYuk/app/src/main/java/com/algokelvin/sehatyuk/MainActivity.java
+++ SehatYuk/app/src/main/java/com/algokelvin/sehatyuk/MainActivity.java
@@ -1,16 +1,27 @@
 package com.algokelvin.sehatyuk;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.algokelvin.sehatyuk.auth.HealthKitAuthActivity;
+import com.algokelvin.sehatyuk.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.btnAuthorize.setOnClickListener(v -> {
+            Intent toAuthorize = new Intent(this, HealthKitAuthActivity.class);
+            startActivity(toAuthorize);
+        });
     }
 }
